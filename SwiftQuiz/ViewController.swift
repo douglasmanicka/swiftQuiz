e868703c2c4437c988e92a35e330c3e05bd5ab5e
douglas manicka <douglasmanicka@gmail.com>
2020-03-05 20:39:39 -0300

    Add launchscreen/assets

---
+++ SwiftQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftQuiz
+//
+//  Created by Douglas Manicka on 05/03/20.
+//  Copyright © 2020 Douglas Manicka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
